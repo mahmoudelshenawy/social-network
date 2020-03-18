573e623cc9e0de2f676d7ec337520109e3964a6f
mahmoud <saqrmadara2000@gmail.com>
2020-03-19 00:33:14 +0200

    prepare to deploy

--- server.js
+++ server.js
@@ -22,13 +22,18 @@ if (process.env.NODE_ENV === "production") {
 	//set the static folder
 	app.use(express.static("client/build"));
 
+	app.use(express.static("client/build"));
+
 	app.get("*", (req, res) => {
-		let url = path.join(__dirname, "./client/build", "index.html");
-		if (!url.startsWith("/app/"))
-			// since we're on local windows
-			url = url.substring(1);
-		res.sendFile(url);
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
+	// app.get("*", (req, res) => {
+	// 	let url = path.join(__dirname, "client/build", "index.html");
+	// 	if (!url.startsWith("/app/"))
+	// 		// since we're on local windows
+	// 		url = url.substring(1);
+	// 	res.sendFile(url);
+	// });
 }
 const PORT = process.env.PORT || 5000;
 
